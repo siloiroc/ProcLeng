4b8e6317f29f02150269ab1e917dd0e8e34f1a4f
coriol <coriol@gmail.com>
2014-08-21 02:04:01 +0200

    Primeras pruebas de verificación de existencia de sentencia Return en funciones. Aun no está limpio y da errores.

--- src/compiler/syntax/nonTerminal/BloqueSentencias.java
+++ src/compiler/syntax/nonTerminal/BloqueSentencias.java
@@ -2,4 +2,25 @@ package compiler.syntax.nonTerminal;
 
 public class BloqueSentencias extends NonTerminal {
 
+	private boolean existeReturn; 
+	
+	public BloqueSentencias() {
+		super();
+	}
+
+	/**
+	 * @return the existeReturn
+	 */
+	public boolean isExisteReturn() {
+		return existeReturn;
+	}
+
+	/**
+	 * @param existeReturn the existeReturn to set
+	 */
+	public void setExisteReturn(boolean existeReturn) {
+		this.existeReturn = existeReturn;
+	}
+
+
 }
