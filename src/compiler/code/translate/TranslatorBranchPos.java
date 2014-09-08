0f1a8b1c3d4baaae24530505a20fc6b40a5c2fa2
coriol <coriol@gmail.com>
2014-09-08 15:39:29 +0200

    TestCase B04 funcionando: Variables y aritmética Simple. Falta comprobar el OR

--- src/compiler/code/translate/TranslatorBranchPos.java
+++ src/compiler/code/translate/TranslatorBranchPos.java
@@ -1,5 +1,8 @@
 package compiler.code.translate;
 
+import compiler.intermediate.Label;
+import compiler.intermediate.Temporal;
+import es.uned.lsi.compiler.intermediate.OperandIF;
 import es.uned.lsi.compiler.intermediate.QuadrupleIF;
 
 public class TranslatorBranchPos extends Translator {
@@ -9,12 +12,22 @@ public class TranslatorBranchPos extends Translator {
 	public String translate(QuadrupleIF quadruple) {
 		//Quadrupla --> ("BP", temporalExpr, labelTrue);
 		StringBuffer b = new StringBuffer();	
+		OperandIF operandResult = quadruple.getResult();
 		String strResult = translate(quadruple.getResult());
-		String strOperand1 = translate(quadruple.getFirstOperand());
-					
-		b.append("\t\t\t\tCMP #1, " + strResult + "\n"); 		//Comparamos el valor del temporal que nos llega de la cuádrupla (será un temporal) con 1, para ver si son iguales	
-		b.append("\t\t\t\tBZ /" + strOperand1);			//Si es 0 (registro Z=1), debemos saltar a la etiqueta indicada por el operando1 (etiqueta de True)
-		return b.toString();
+		if (operandResult instanceof Label){
+			
+			b.append("\t\t\t\tBP /" + strResult);
+		}
+		else if (operandResult instanceof Temporal){
+			
+			String strOperand1 = translate(quadruple.getFirstOperand());
+						
+			b.append("\t\t\t\tCMP #1, " + strResult + "\n"); 		//Comparamos el valor del temporal que nos llega de la cuádrupla (será un temporal) con 1, para ver si son iguales	
+			b.append("\t\t\t\tBZ /" + strOperand1);					//Si es 0 (registro Z=1), debemos saltar a la etiqueta indicada por el operando1 (etiqueta de True)
+		}
+		return b.toString();		
+		
+		
 	}
 
 }
